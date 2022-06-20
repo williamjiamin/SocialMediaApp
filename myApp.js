687893ec58b725d043497b26d7371047f0db510e
williamjiamin <lipai_wang@163.com>
2022-06-20 21:26:51 +0800

    lexueoude.com

--- myApp.js
+++ myApp.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    // console.log("Connected to our Node server.");
+
+    const url = req.url;
+
+    if(url == "/profile"){
+        res.setHeader('Content-type','text/html');
+        res.write("<h1>This is lexueoude.com's profile page</h1>");
+        return res.end();
+    }
+
+    res.setHeader('Content-type','text/html');
+    res.write("<h1>Welcome  to lexueoude.com's other page.</h1>");
+    res.end();
+
 
 })
 
